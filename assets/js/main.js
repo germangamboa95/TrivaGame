b9edbef796b33b4ec4b6be68e1dfbfff52e83c00
German Gamboa <germangamboa95@gmail.com>
2018-04-03 19:01:38 -0400

    basic game is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,70 +1,36 @@
-console.log("Hello World");
-
-//Construct current quiz object
-
-
-
 window.onload = () => {
   // Import data
-  fetch("game.json")
+  fetch("strange.json")
     .then(res => res.json())
     .then(res => appCtrl(res));
 
-  //Controller function???
   function appCtrl(data) {
       
     let selection;
 
     const quiz = new Quiz(data);
 
-    quiz.nextQuestion();
 
+    //Quiz Name 
     $(".title").text(quiz.title);
-
 
     // UI highlight
     $('.list-group').on('click','li', selector);
+   
 
     // Next btn
     $('#btn').on('click', function(){
-        quiz.nextQuestion(selection);
+
+        selection = quiz.nextQuestion(selection);
+    
 
     });
 
-
-
-
-
-    //UI functions below
-    function dispResults(res) {
-        console.log(res);
-        let markup = `
-            <ul class="list-group py-2">
-            <li  class="list-group-item"> You answered ${res.correct} questions correctly</li>
-            <li  class="list-group-item"> You answered ${res.wrong} questions incorrectly</li>
-            <li  class="list-group-item"> Your score is: ${res.percent * 100}% </li>
-            </ul>
-        `;
-        $('.card-text').html(markup);
-    }
+    //UI helper function'
     function selector() {
         (selection)? selection.classList.remove('selected'): null;
         selection = this;
         selection.classList.add('selected');
      }
-
-    function updateUi() {
-        $('.counter').text(`${quiz.questionCounter + 1} of ${quiz.quizLength}`);
-        $('.list-group').html(' ');
-        $(".question").text(quiz.currentQuestion.question_string);
-        for (let i in quiz.currentQuestion.options) {
-          let option = quiz.currentQuestion.options[i];
-          $(".list-group").append(
-            `<li value="${i}" class="list-group-item">${i}: ${option}</li>`
-          );
-        }
-      }
-
   }
-
 };
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -12,19 +12,24 @@ class Quiz {
     this.timeLeft = this.timer;
     this.timerCountDown();
     this.timerData;
+    this.updateUi();
   }
 
   timerCountDown() {
     this.timerData = setInterval(() => {
       this.timeLeft -= 1000;
       console.log(this.timeLeft);
-      this.timeLeft <= 0 ? this._endQuiz() : null;
+      $('.timer').text(this.timeLeft / 1000 + ' Seconds' );
+      if(this.timeLeft < 0) {
+        let res = this._endQuiz();
+        this.dispResults(res, true);
+      }
     }, 1000);
   }
 
   checkAnswer(input) {
-    const res = this.questions[this.questionCounter].answer == input;
-    console.log(res);
+    const res = this.questions[this.questionCounter].answer === input;
+    console.log(input,' ', res);
     this._updateCounters(res);
     return res;
   }
@@ -40,26 +45,34 @@ class Quiz {
       percent: this.right / this.quizLength
     };
     clearTimeout(this.timerData);
+    this.gihpy(obj);
     return obj;
   }
 
   nextQuestion(selection) {
-    let select;
-    selection ? console.log((select = selection.attributes.value.value)) : null;
 
-    if (this.questionCounter == this.quizLength) {
+    let select = selection? selection.attributes.value.value : false;
+
+    this.checkAnswer(select);
+
+    if (this.questionCounter > this.quizLength -1) {
+
       let res = this._endQuiz();
+
       this.dispResults(res);
+
     } else {
-      this.checkAnswer(select);
-      this.updateUi();
+
       this.currentQuestion = this.questions[this.questionCounter];
+
+      this.updateUi();
     }
     
+    return null;
   }
 
   updateUi() {
-    $(".counter").text(`${this.questionCounter} of ${this.quizLength}`);
+    $(".counter").text(`${this.questionCounter +1 } of ${this.quizLength}`);
     $(".list-group").html(" ");
     $(".question").text(this.currentQuestion.question_string);
     for (let i in this.currentQuestion.options) {
@@ -70,20 +83,58 @@ class Quiz {
     }
   }
 
-  dispResults(res) {
+  dispResults(res, flag = false) {
     console.log(res);
     let markup = `
-          <ul class="list-group py-2">
-          <li  class="list-group-item"> You answered ${
+          <ul class="list-group text-center py-2">
+          <li  class="py-2"> You answered ${
             res.correct
           } questions correctly</li>
-          <li  class="list-group-item"> You answered ${
+          <li  class="py-2"> You answered ${
             res.wrong
           } questions incorrectly</li>
-          <li  class="list-group-item"> Your score is: ${res.percent *
+          <li  class="py-2"> Your score is: ${res.percent *
             100}% </li>
           </ul>
       `;
+    if(flag) $(".card-text").before('<h2  class="alert alert-danger text-center" role="alert">You ran out of time!</h2>');  
+
     $(".card-text").html(markup);
   }
+
+  gihpy(res){
+    let query;
+    if(res.percent == 0){
+      query = 'failure';
+    } else if(res.percent == 1){
+      query = 'awesome';
+    } else if(res.percent >= .5 && res.percent < .6) {
+      query = 'depression';
+    } else if(res.percent > 0 && res.percent < .1) {
+      query = 'boo';
+    } else if(res.percent >= .1 && res.percent < .2) {
+      query = 'cries';
+    } else if(res.percent >= .3 && res.percent < .4) {
+      query = 'damn';
+    } else if(res.percent >= .6 && res.percent < .7) {
+      query = 'meh';
+    } else if(res.percent >= .7 && res.percent < .8) {
+      query = 'alright';
+    } else if(res.percent >= .8 && res.percent < .9) {
+      query = 'YES!';
+    } else if(res.percent >= .9 && res.percent < 1) {
+      query = 'win';
+    }
+
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=YqLensbIWv5skyGVSr6ZPFClfQImMmX4&q='+query+'&limit=10&offset=0&rating=R&lang=en')
+      .then(res => res.json())
+      .then(data => this.addPic(data));
+  }
+
+  addPic(data) {
+    console.log(data);
+    let rand = Math.floor(Math.random() * 10);
+    $(".card-text").append(`<img class="mx-auto d-block pb-3 img-fluid" src="${data.data[rand].images.downsized_medium.url}">`);
+  }
+
 }
