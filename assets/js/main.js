92eb97e4e1818048c3339cea2d10d7ec15c871c1
German Gamboa <germangamboa95@gmail.com>
2018-04-03 02:34:50 -0400

    separated files

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,55 +2,7 @@ console.log("Hello World");
 
 //Construct current quiz object
 
-class Quiz {
-  constructor(data) {
-    this.title = data.name;
-    this.timeLimit = data.time;
-    this.questions = data.questions;
-    this.quizLength = data.questions.length;
-    this.wrong = 0;
-    this.right = 0;
-    this.questionCounter = 0;
-    this.currentQuestion = data.questions[0];
-  }
 
-  checkAnswer(input) {
-    const res =this.questions[this.questionCounter].answer == input;
-    this._updateCounters(res);
-    return res;
-  }
-
-  _updateCounters(res) {
-    if (res) {
-      this.right++;
-    } else {
-      this.wrong++;
-    }
-
-    this.questionCounter++;
-  }
-  _endQuiz(){
-
-      let obj = {
-        correct: this.right,
-        wrong: this.wrong,
-        percent: this.right/this.quizLength
-      };
-      return obj;
-  }
-  nextQuestion() {
-      if(this.questionCounter == this.quizLength) {
-          return this._endQuiz();
-      } else {
-        this.currentQuestion = this.questions[this.questionCounter];
-      }
-  }
-
-  //Test function
-  dataDump() {
-    console.log(this.wrong + " " + this.right);
-  }
-}
 
 window.onload = () => {
   // Import data
