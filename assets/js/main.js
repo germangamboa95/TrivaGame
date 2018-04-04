9fd0e2b4d8c41f321b93fbda77715b2ec96a95c0
German Gamboa <germangamboa95@gmail.com>
2018-04-04 12:30:44 -0400

    now you can select from quizes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,46 @@
 window.onload = () => {
-  // Import data
-  fetch("strange.json")
-    .then(res => res.json())
-    .then(res => appCtrl(res));
+  $('a').on('click', function(){
+    let quiz = this.attributes.value.value;
+    fetch('quiz.html')
+    .then(res => res.text())
+    .then(data => {
+      $('body').html(data);
+       fetch(quiz)
+      .then(res => res.json())
+      .then(res => appCtrl(res));
 
+    });
+
+
+
+  });
+
+
+  // fetch("strange.json")
+  //   .then(res => res.json())
+  //   .then(res => appCtrl(res));
+
+
+  // Import data
   function appCtrl(data) {
-      
+
     let selection;
 
     const quiz = new Quiz(data);
 
 
-    //Quiz Name 
+    //Quiz Name
     $(".title").text(quiz.title);
 
     // UI highlight
     $('.list-group').on('click','li', selector);
-   
+
 
     // Next btn
     $('#btn').on('click', function(){
 
         selection = quiz.nextQuestion(selection);
-    
+
 
     });
 
